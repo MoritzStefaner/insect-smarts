3925a355205e8e353c016c50e92b8de2d68b466f
Dominikus Baur <dominikus.baur@gmail.com>
2015-04-10 11:31:31 +0200

    added more stuff to library

---
+++ libraries/eclipse-build/distribution/insectsmarts-1/src/template/library/HelloLibrary.java
@@ -0,0 +1,103 @@
+/**
+ * I-N°S.E-C:T S.M-A°R:T.S
+ * A collection of insect inspired algorithms.
+ * http://stefaner.eu/insectsmarts
+ *
+ * Copyright (c) 2013 Moritz Stefaner http://stefaner.eu
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General
+ * Public License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ * Boston, MA  02111-1307  USA
+ * 
+ * @author      Moritz Stefaner http://stefaner.eu
+ * @modified    04/10/2015
+ * @version     1.0.0 (1)
+ */
+
+package template.library;
+
+
+import processing.core.*;
+
+/**
+ * This is a template class and can be used to start a new processing library or tool.
+ * Make sure you rename this class as well as the name of the example package 'template' 
+ * to your own library or tool naming convention.
+ * 
+ * (the tag example followed by the name of an example included in folder 'examples' will
+ * automatically include the example in the javadoc.)
+ *
+ * @example Hello 
+ */
+
+public class HelloLibrary {
+	
+	// myParent is a reference to the parent sketch
+	PApplet myParent;
+
+	int myVariable = 0;
+	
+	public final static String VERSION = "1.0.0";
+	
+
+	/**
+	 * a Constructor, usually called in the setup() method in your sketch to
+	 * initialize and start the library.
+	 * 
+	 * @example Hello
+	 * @param theParent
+	 */
+	public HelloLibrary(PApplet theParent) {
+		myParent = theParent;
+		welcome();
+	}
+	
+	
+	private void welcome() {
+		System.out.println("I-N°S.E-C:T S.M-A°R:T.S 1.0.0 by Moritz Stefaner http://stefaner.eu");
+	}
+	
+	
+	public String sayHello() {
+		return "hello library.";
+	}
+	/**
+	 * return the version of the library.
+	 * 
+	 * @return String
+	 */
+	public static String version() {
+		return VERSION;
+	}
+
+	/**
+	 * 
+	 * @param theA
+	 *          the width of test
+	 * @param theB
+	 *          the height of test
+	 */
+	public void setVariable(int theA, int theB) {
+		myVariable = theA + theB;
+	}
+
+	/**
+	 * 
+	 * @return int
+	 */
+	public int getVariable() {
+		return myVariable;
+	}
+}
+
